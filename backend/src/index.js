581f6d653df47611a3a857db785ef5849fb986a8
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-03 14:52:14 +0700

    Create sign-up endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 
 import { connectToDB } from './db/connectToDB.js'
+import authRoute from './routes/auth.route.js'
 
 dotenv.config()
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5001
+
+app.get('/health', (req, res) => {
+	res.send('App is healthy')
+})
+
+app.use(express.json()) // allows us to parse incoming requests:req.body
+app.use(cookieParser()) // allows us to parse incoming cookies
+
+app.use('/api/auth', authRoute)
 
 app.listen(PORT, async () => {
 	await connectToDB()
-	console.log('Server is running on port: ', PORT)
+	console.log('Server is running on port:', PORT)
 })
