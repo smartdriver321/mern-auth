de8e74313bcedb7b134e503db24b1d1c3749bf22
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-04 13:04:53 +0700

    Create SignUpPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,42 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import SignUpPage from './pages/SignUpPage'
+import FloatingShape from './components/FloatingShape'
+
 export default function App() {
 	return (
-		<>
-			<h1 className='text-red-500'>Hello from App.jsx</h1>
-		</>
+		<div
+			className='min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'
+		>
+			<FloatingShape
+				color='bg-green-500'
+				size='w-64 h-64'
+				top='-5%'
+				left='10%'
+				delay={0}
+			/>
+			<FloatingShape
+				color='bg-emerald-500'
+				size='w-48 h-48'
+				top='70%'
+				left='80%'
+				delay={5}
+			/>
+			<FloatingShape
+				color='bg-lime-500'
+				size='w-32 h-32'
+				top='40%'
+				left='-10%'
+				delay={2}
+			/>
+
+			<Routes>
+				<Route path='/' element={'Home'} />
+				<Route path='/sign-up' element={<SignUpPage />} />
+				<Route />
+			</Routes>
+		</div>
 	)
 }
