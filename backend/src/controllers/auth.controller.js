ee389df898d567d368ba8d02491a1439a2c797bb
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-04 10:27:36 +0700

    Create check-auth endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -10,6 +10,20 @@ import {
 } from '../mailtrap/emails.js'
 import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js'
 
+export const checkAuth = async (req, res) => {
+	try {
+		const user = await User.findById(req.userId).select('-password')
+		if (!user) {
+			return res.status(400).json({ success: false, message: 'User not found' })
+		}
+
+		res.status(200).json({ success: true, user })
+	} catch (error) {
+		console.log('Error in check auth ', error)
+		res.status(400).json({ success: false, message: error.message })
+	}
+}
+
 export const signUp = async (req, res) => {
 	const { email, password, name } = req.body
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,8 @@
 import express from 'express'
 
+import { verifyToken } from '../middleware/verifyToken.js'
 import {
+	checkAuth,
 	forgotPassword,
 	resetPassword,
 	signIn,
@@ -10,6 +12,8 @@ import {
 } from '../controllers/auth.controller.js'
 
 const router = express.Router()
+
+router.get('/check-auth', verifyToken, checkAuth)
 
 router.post('/sign-up', signUp)
 router.post('/sign-in', signIn)
