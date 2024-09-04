355d7152faa09a9f13008e6fc5675ed634034a4c
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-04 21:21:35 +0700

    Preparing for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
 
 import { connectToDB } from './db/connectToDB.js'
 import authRoute from './routes/auth.route.js'
@@ -10,6 +11,7 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 app.get('/health', (req, res) => {
 	res.send('App is healthy')
@@ -20,6 +22,14 @@ app.use(express.json()) // allows us to parse incoming requests:req.body
 app.use(cookieParser()) // allows us to parse incoming cookies
 
 app.use('/api/auth', authRoute)
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+	})
+}
 
 app.listen(PORT, async () => {
 	await connectToDB()
